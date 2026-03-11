22457cbff1dd1f3bd3adaed7082f3c869dd0a920
Om Patil <patilomh@gmail.com>
2026-03-11 23:15:50 +0530

    Problem 1 - Person class implementation

---
+++ 01_Person.java
@@ -0,0 +1,25 @@
+// 1.Create a class called Person with name (String) and age (int) attributes. Implement
+// a constructor to set these attributes upon object creation. Create two Person objects,
+// set their details, and print them.
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Om", 20);
+        Person p2 = new Person("Rahul", 22);
+
+        p1.display();
+        p2.display();
+    }
+}
